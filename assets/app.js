7ebf8f663ab7707ebbdebb5ab80463f4e2bef82c
Josh Parker <jthomasparker@gmail.com>
2018-03-23 15:56:28 -0400

    added favorites and data persistence, code still needs revising

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,6 @@
-//var btnState;
-//var dataState;
+// TODO: sticky header, clear all shows button, ability to save favorite gifs
+// reduce redundancy (ie checking the button state on 2 different click events could possibly be combined into one function)
+
 var q;
 var limit = 10;
 var apiKey = "96q4xCDQ26HgzN3CHLgjM7oJuSUnYffu"
@@ -7,30 +8,38 @@ var topicTitle;
 var header = $("#header")
 var sticky = header.offsetTop;
 var colorSwitch = false;
-var storedArr = [];
-
-
-
-
-
 var topics = ["The Office", "New Girl", "It's Always Sunny in Philadelphia", "Modern Family", "Parks and Recreation", "Arrested Development"];
+var favorites = [];
+var firstAdd = false;
+var showFavs = false;
+
 
 $(document).ready(function(){
     // checks local storage for our topics array
     if(localStorage["storedTopics"]){
+        // if it's there, retrieve it
         var localData = localStorage.getItem("storedTopics");
         topics = JSON.parse(localData)
     } else {
+        // if it's not there, send the topics array to localstorage
         updateLocalStorage("storedTopics", topics)
     }
 
+    if(localStorage["storedFavorites"]){
+        var localData = localStorage.getItem("storedFavorites");
+        favorites = JSON.parse(localData)
+    } else {
+        updateLocalStorage("storedFavorites", favorites)
+    }
+    if(favorites.length > 0){
+        showFavs = true
+        firstAdd = true
+    }
+
+    toggleFavPanel();
+
     // loads topic buttons
     loadTopics();
-
-
-    window.onscroll = function(){
-      //  stickyHeader();
-    }
 
 // Click Events:
 
@@ -56,6 +65,7 @@ $(document).ready(function(){
    })
 
 
+
    // add topic button click event
     $("#add-topic").on('click', function(){
         addTopic();
@@ -68,9 +78,46 @@ $(document).ready(function(){
             addTopic();
         }
     })
-    
+
+    // favorite button click event
+    $('body').on('click', '.btnFavorite', function(){
+        var thisBtn = $(this);
+        addToFav(thisBtn)
+        
+    })
+
+    $('body').on('click', '.toggle-favs', function(){
+        if($(this).attr('id') === 'show-favs'){
+        showFavs = true;
+        }else{
+            showFavs = false;
+        }
+        toggleFavPanel();
+    })
+
+    $('body').on('click', '.btnRemove', function(){
+           var Id = $(this).attr("id")
+           Id = Id.substr(3)
+            var btn = $('#' + Id)
+            console.log(btn.length)
+            if(btn.length > 0){
+                alert("here")
+                addToFav(btn)
+            
+            } else {
+                alert('there')
+                var idx = favorites.indexOf(Id)
+                favorites.splice(idx, 1)
+              //  addToFav(btn)
+                updateLocalStorage("storedFavorites", favorites)
+                processFavorites()
+            }
+        
+    })
     
 })
+
+
 
 
 // creates a button for each item in the topics array
@@ -87,35 +134,44 @@ function loadTopics(){
     }
 }
 
+// updates the local storage with the topics array
 function updateLocalStorage(key, value){
-
-   // console.log(JSON.parse(localStorage[key]).length)
     if(localStorage[key]){
         localStorage.removeItem(key);
     }
-        localStorage.setItem(key, JSON.stringify(value));
-    
+        localStorage.setItem(key, JSON.stringify(value));  
 }
+    
 
 
+// adds a topic button and saves it to the topics array
 function addTopic(){
+
     // get the textbox value
     var topicToAdd = $("#topic-input").val().trim();
+    
     // if the textbox isn't empty, add a new btnTopic button
     if(topicToAdd.length > 0){
         $("<button>")
         .attr({"class": "btn btn-primary btnTopic", "dataTopic": topicToAdd, "btnState": "inactive"})
         .text(topicToAdd)
         .appendTo("#categories")
+        
+        //update topics array
         topics.push(topicToAdd)
-        //update local storage
+
+        //update local storage with the topics array
         updateLocalStorage("storedTopics", topics)
-        // clean up when you're done
+
+        // clean up
         $("#topic-input").val("")
     }
 }
 
+
+// handles the click event for a topic button
 function btnTopicClick(thisBtn){
+
      // store topic for panel titles
     topicTitle = $(thisBtn).attr("dataTopic");
     
@@ -155,8 +211,9 @@ function btnTopicClick(thisBtn){
  
      
 
-
+// queries the API
 function queryAPI(){
+
     // build the url for the api call
     var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&limit=" + limit
     queryURL += "&q=" + q
@@ -169,6 +226,7 @@ function queryAPI(){
 
         // store the results
         var results = response.data;
+        console.log(results)
         
         // create the panel that will hold the resulting gifs
         var topicPanel = $('<div>').attr({
@@ -188,25 +246,60 @@ function queryAPI(){
             
             // alternates the color of the panel headers
             if(colorSwitch){
-                panelHeading.css('background', '#a63a50')
+                panelHeading.css({
+                    'background': '#a63a50',
+                    'border-color': '#a63a50'
+                    })
                 colorSwitch = false;
             } else {
                 colorSwitch = true;
             }
 
-        // create a div to wrap the body to make it collapsible
-        var collapse = $('<div class="panel-collapse collapse in">').attr("id", "collapse-" + q)
+        // glyph to show collapsed/not collapsed
+        var glyphSpan = $('<span>')
+                        .html("<p>")
+                        .appendTo(panelHeading)
+
+        var glyph = $('<i class="glyphicon glyphicon-chevron-up">')
+                    .appendTo(glyphSpan)
+        
+
+        // create a div to wrap the panel body to make it collapsible, append it to the panel
+        var collapseDiv = $('<div class="panel-collapse collapse in">')
+                        .attr('id', "collapse-" + q)
                         .appendTo(topicPanel)
-        // create panel body, append it to the panel
+                        // change glyph based on whether or not the panel is collapsed
+                        .on('shown.bs.collapse', function() {
+                            glyph.addClass('glyphicon-chevron-up').removeClass('glyphicon-chevron-down')})
+                        .on('hidden.bs.collapse', function() {
+                            glyph.addClass('glyphicon-chevron-down').removeClass('glyphicon-chevron-up')});
+
+        // create panel body, append it to the collapse div
         var panelBody = $('<div class="panel-body">')
-                        .appendTo(collapse)
+                        .appendTo(collapseDiv)
 
         // loop through the results and get what we need
         for(var i = 0; i < results.length; i++){
+            var gifID = results[i].id
             // label for the gif (Rating for now, may add title later)
-           var label = $('<span>').html("<b>Rating: " + results[i].rating.toUpperCase() + "</b><br>");
+            var labelDiv = $('<div>')
+          // var label = $('<span>').html("<b>Rating: " + results[i].rating.toUpperCase() + "</b>");
+          var label = "<b> Rating: " + results[i].rating.toUpperCase() + "</b>"
+         //  var favStar = $('<span class="glyphicon glyphicon-star">')
+            var btnFavorite = $('<button class="btn btn-default btnFavorite">')
+                            .attr('id', gifID)
+            
+                            
+
+                          labelDiv.html(label)  
+            updateFavBtn(btnFavorite) 
+            labelDiv.prepend(btnFavorite) 
+         // label.prepend(btnFavorite)
+          
+
             // create the img element for the gif
             var resultImage = $("<img>");
+
             // assign attributes to the img from the results
             resultImage.attr({
                 'src': results[i].images.fixed_height_still.url, 
@@ -214,39 +307,157 @@ function queryAPI(){
                 'animate-url': results[i].images.fixed_height.url,
                 'still-url': results[i].images.fixed_height_still.url,
                 'class': "gif",
-                'alt': results[i].title
+                'alt': results[i].title,
+                'id': "#gif-" + gifID
                 });
             
             // create a div for each gif with the label and gif
+          //  label.prepend(btnFavorite)
+           // label.append(resultImage)
+            
             var resultDiv = $('<div class="resultDiv">')
-                        .append(label)
-                        .append(resultImage)
+                        .append(labelDiv, resultImage)
+                       // .append(resultImage)
 
-            // add the result div to the panel
+            // add the result div to the panel body
             panelBody.prepend(resultDiv)
         }
 
-        // add the panel to #results
+        // prepend the panel to #results
         $("#results").prepend(topicPanel);
     })
 }
 
 
 
+function processFavorites(){
+    $('#favBody').empty();
+      if(showFavs && favorites.length > 0){  
+        var queryURL = "https://api.giphy.com/v1/gifs?api_key=" + apiKey + "&ids=" + favorites
+        $.ajax({
+            url: queryURL,
+            method: 'GET'
+        }).done(function(response){
+            var results = response.data
+            
+            for(i=0; i < results.length; i++){
+                // create the img element for the gif
+                var resultImage = $("<img>");
 
-/* function to add/remove sticky class for sticky header
-Can't get it to work, will have to come back
-function stickyHeader(){
-    
-    var header = document.getElementById("sticky-header")
-    var sticky = header.offsetTop;
-    console.log(header.offsetTop)
-    if(window.pageYOffset >= sticky){
-        header.classList.add("sticky");
-    } else {
-        header.classList.remove("sticky");
+                // assign attributes to the img from the results
+                resultImage.attr({
+                    'src': results[i].images.fixed_height_still.url, 
+                    'dataState': "still",
+                    'animate-url': results[i].images.fixed_height.url,
+                    'still-url': results[i].images.fixed_height_still.url,
+                    'class': "gif",
+                    'alt': results[i].title,
+                    'id': "#fav-" + results[i].id
+                });
+
+                var Id = 'rmv' + results[i].id
+                var resultDiv = $('<div class="resultDiv">')
+                var favRemove = $('<span class="glyphicon glyphicon-remove">')
+                var btnRemove = $('<button class="btn btn-default btnRemove">')
+                                .attr("id", Id)
+                                    
+                
+
+                btnRemove.append(favRemove)
+                
+                
+               resultDiv.append(btnRemove, resultImage)
+                $('#favBody').prepend(resultDiv)
+            }   
+        })
     }
 }
-*/
 
 
+function updateFavBtn(thisBtn){
+    thisBtn.empty();
+    var Id = $(thisBtn).attr("id");
+    var btnState = $(thisBtn).attr("btnState")
+    var favStar = $('<span class="glyphicon">')
+    if(favorites.indexOf(Id) < 0){
+        $(thisBtn).attr("btnState", "inactive")
+        favStar.removeClass("glyphicon-star").addClass("glyphicon-star-empty")
+       
+    } else {
+        $(thisBtn).attr("btnState", "active")
+        favStar.removeClass("glyphicon-star-empty").addClass("glyphicon-star")
+        
+    }
+    $(thisBtn).append(favStar)
+    }
+
+
+
+function addToFav(thisBtn){
+    var btnID = $(thisBtn).attr("id")
+    var favStar = $('<span class="glyphicon glyphicon-star">')
+        if(favorites.indexOf(btnID) < 0){
+            $(thisBtn).attr("btnState", "active")
+            favorites.push(btnID)
+        } else {
+            $(thisBtn).attr("btnState", "inactive")
+            var idx = favorites.indexOf(btnID)
+            favorites.splice(idx, 1)
+        }
+        // add this to remove
+        var favCount = favorites.length;
+        $('#show-favs')
+        .html(" (" + favCount + ")")
+        .prepend(favStar)
+        updateFavBtn(thisBtn)
+      //  toggleFavPanel()
+        updateLocalStorage("storedFavorites", favorites)
+        processFavorites()
+        if(favCount === 0){
+            showFavs = false;
+            toggleFavPanel()
+        }
+        if(!firstAdd){
+            firstAdd = true;
+            showFavs = true;
+            toggleFavPanel();
+        }
+}
+
+function toggleFavPanel(){
+    var favCount = favorites.length;
+    var favStar = $('<span class="glyphicon glyphicon-star">')
+    if(showFavs && favCount > 0){
+        $('#show-favs').hide()
+        $('#results').addClass('col-md-8')
+        $('#favorites').removeClass('col-xs-0').addClass('col-xs-12 col-md-4')
+        
+        var favPanel = $('<div>')
+            .attr({
+            'class': "panel panel-default",
+            'id': "favPanel"
+            })
+        
+        var favHeading = $('<div class="panel-heading">').appendTo(favPanel)
+        var favTitle = $('<h3 class="panel-title">').html("Favorites<br>").appendTo(favHeading)
+        var hideFavs = $('<button class="btn btn-default toggle-favs">')
+                        .text(" Hide")
+                        .attr("id", "hide-favs")
+                        .appendTo(favTitle);
+        
+        hideFavs.prepend(favStar)
+        var favBody = $('<div class="panel-body" id="favBody">').appendTo(favPanel)
+        favPanel.appendTo("#favorites")
+        firstAdd = true;
+        processFavorites();
+        } else {
+            
+            $('#show-favs')
+        .html(" (" + favCount + ")")
+        .prepend(favStar)
+        .show()
+            $('#favorites').empty();
+            $('#results').removeClass('col-md-8')
+            $('#favorites').removeClass('col-xs-12 col-md-4').addClass('col-xs-0')
+        }
+    }
